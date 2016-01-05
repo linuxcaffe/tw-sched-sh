171af9a5c23c8a4d59076a19baec84cd65a18eaf
djp <davamundo@gmail.com>
2016-01-04 22:42:09 -0500

    refactoring after getopts, part 1

--- tsch.sh
+++ tsch.sh
@@ -137,29 +137,27 @@ TASK_OPTS="rc.verbose: rc.defaultwidth:${WIDTH}"
 
 DIVIDER='-------------------------------------'
 MODE='sched' # sched, resched, unsched
-RPT_READY_UUID='rc.report.ready.columns=uuid rc.report.ready.labels= ready'
+#RPT_READY_UUID='rc.report.ready.columns=uuid rc.report.ready.labels= ready'
 
 # LIST_DESC='rc.report.list.columns=description,scheduled rc.report.list.labels= list'
 #CONTEXT_LIST=$(grep ^context. $TASK_RC | cut -d '.' -f 2 | cut -d '=' -f 1)
 
 CAND_LIST_LIMIT=$($TASK $TASK_OPTS _get rc.sched.cand.list.limit)
+CAND_COUNT=$($TASK $TASK_OPTS tag.not:nosch rc.context:$CONTEXT $FILTER +READY count)
 PENDING_COUNT=$($TASK $TASK_OPTS rc.context: +PENDING count)
 READY_COUNT=$($TASK $TASK_OPTS rc.context: +READY +PENDING count)
 SCHED_NONE_COUNT=$($TASK $TASK_OPTS +PENDING $FILTER +READY scheduled.none: count)
 SCHED_OLD_COUNT=$($TASK $TASK_OPTS +PENDING $FILTER +READY scheduled.before:today count)
 TARGET_LIST_LIMIT=$($TASK $TASK_OPTS _get rc.sched.target.list.limit)
+TARGETS_COUNT=$(task $FILTER +PENDING scheduled.after:now count)
+TARGET_NEXT_ID=$(task rc.report.sch_target.columns=id rc.report.sch_target.labels= limit:1 sch_target)
 
 # function: get_candidates
-CANDIDATES=$($TASK $TASK_OPTS tag.not:nosch rc.context:$CONTEXT $FILTER +READY $RPT_READY_UUID)
-CANDIDATES_COUNT=$($TASK $TASK_OPTS tag.not:nosch rc.context:$CONTEXT $FILTER +READY count)
+#CAND=$($TASK $TASK_OPTS tag.not:nosch rc.context:$CONTEXT $FILTER +READY $RPT_READY_UUID)
 
 # function: get_next_target
-TARGET_DATE_RANGE='sched.after:now'
-TARGETS=$($TASK $TASK_OPTS $FILTER $TARGET_DATE_RANGE uuids)
-TARGETS_COUNT=$(task $FILTER +PENDING scheduled.after:now count)
-TARGET_ID=$(task $FILTER +PENDING scheduled.after:now limit:1 ids)
-
-TARGET_ID=$(echo $TARGETS |cut -d' ' -f1)
+# TARGET_DATE_RANGE='sched.after:now'
+# TARGETS=$($TASK $TASK_OPTS $FILTER $TARGET_DATE_RANGE uuids)
 # TARGET_NEXT_DESC=`$TASK $TASK_OPTS $TARGET_NEXT $LIST_DESC`
 
 ## COLORS
@@ -211,15 +209,20 @@ declare -A warning=([value]="[32;40m" [alt]="^[[38;5;242m" [end]="[0m")  # yel
     #echo -e $DIVIDER
 
 ## Candidates list 
-if [[ "$BATCH_MODE" == off ]]; then
-    echo -e $C2' Next of '$CANDIDATES_COUNT' Candidates'$Cx
-    CAND_ID=`task rc.verbose=label rc.context:$CONTEXT $FILTER limit:1 ids`;
+if [[ "$BATCH_MODE" == 0 ]]; then
+    if [[ $CAND_COUNT == '0' ]]; then
+	echo -e ' No tasks match! Try changing the context or filter'
+	exit 1
+    else
+    echo -e $C2' Next of '$CAND_COUNT' Candidates'$Cx
+#    CAND_ID=`task rc.verbose=label rc.context:$CONTEXT $FILTER limit:1 ids`;
     task rc.verbose=label rc.context:$CONTEXT $FILTER limit:1 sch_cand;
-elif [[ "$BATCH_MODE" == on ]]; then
-    echo -e $C2' '$CANDIDATES_COUNT' Candidates'$Cx
+    fi
+elif [[ "$BATCH_MODE" == 1 ]]; then
+    echo -e $C2' '$CAND_COUNT' Candidates'$Cx
 	task rc.verbose=label rc.context:$CONTEXT $FILTER limit:$CAND_LIST_LIMIT sch_cand;
-    if [[ "$CANDIDATES_COUNT" -gt "$CAND_LIST_LIMIT" ]]; then
-	CAND_MORE=$(( $CANDIDATES_COUNT - $CAND_LIST_LIMIT ))
+    if [[ "$CAND_COUNT" -gt "$CAND_LIST_LIMIT" ]]; then
+	CAND_MORE=$(( $CAND_COUNT - $CAND_LIST_LIMIT ))
     echo -e $C3' (and '$CAND_MORE' more) are matching, unscheduled tasks'$Cx; else
     echo -e $C3' these are matching, unscheduled tasks'$Cx
     fi
@@ -239,10 +242,10 @@ fi
 	echo -e $DIVIDER
     echo ' (enter target-ID#, date or "y" or "n" or "q"'
     read -ep $C1' Schedule > '$Cx prompt
-if [[ "$prompt" == '' ]] && [[ "$TARGET_ID" == '' ]]; then
+if [[ "$prompt" == '' ]] && [[ "$TARGET_NEXT_ID" == '' ]]; then
     echo ' No targets found, please enter a date'
     # TODO cycle prompt
-elif [[ "$prompt" == '' ]] && [[ "$TARGET_ID" = [0-9]+ ]]; then
+elif [[ "$prompt" == '' ]] && [[ "$TARGET_NEXT_ID" = [0-9]+ ]]; then
     TASK_CMD=`echo -e 'task '$CAND_ID' mod sched:'$CAND_ID'.scheduled'`
     echo $TASK_CMD
     read -ep ' (Y/n)' conf_prompt
@@ -276,7 +279,7 @@ if [[ "$DBG" == on ]]; then
     echo 'TARGET next = '$TARGET_NEXT;
     echo 'TARGET next desc = '$TARGET_NEXT_DESC;
     echo 'TARGETS count = '$TARGETS_COUNT;
-    echo 'CANDIDATES count = '$CANDIDATES_COUNT; 
+    echo 'CAND count = '$CAND_COUNT; 
     echo 'UNSCHEDULED count = '$SCHED_NONE_COUNT;
     echo '============ND DEBUG==========';
 fi
