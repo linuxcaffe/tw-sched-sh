e071e17592837349f4d58fa800fa543d77ee8875
Harley Pig <harleypig@gmail.com>
2016-01-08 16:36:47 -0700

    made most of the changes in cosmetic_and_style branch, moved a lot of stuff to functions to clean up some flow

--- tsch.sh
+++ tsch.sh
@@ -41,6 +41,8 @@ EOT
     exit 1
 fi
 
+TASK_OPTS="rc.verbose: rc.defaultwidth:90"
+
 ##################################################################################
 # Help Setup
 
@@ -68,19 +70,112 @@ at the Schedule > prompt;
 
 "
 
-usage () { echo "$USAGE" ; echo -e "!!! $@" ; exit 1 ; }
+##################################################################################
+# Color setup
+
+# Themse could be allowed here by changing this to something like
+# HEAD_TEXT_COLOR=${HEAD_TEXT_COLOR:=$(tput bold ; tput setaf 2)
+# Then, if the config file doesn't have this value set, the default will work.
+#
+# I'm defaulting tput to 8 colors output. Testing and handling for 256 colors
+# can come later.
+
+BOLD=$(tput bold)
+GREEN_FG=$(tput setaf 2)
+WHITE_FG=$(tput setaf 7)
+BLACK_FG=$(tput setaf 0)
+RESET=$(tput sgr0)
+
+##################################################################################
+# Functions
+
+# XXX: Convert system calls to 'runit' for error handling.
+# XXX: Add option to log output to file instead of stdout.
+
+# These two are cut-n-paste from my snippets collection. They need to be
+# tweaked for each situation.
+#
+# LOGIT_PREFIX is created on program start and can be as simple or complex as
+# you want.
+#
+# NOTREALLY is used for debugging and dry-runs.
+#
+#logit { echo "${LOGIT_PREFIX} $@" >> $LOGFILE ; }
+#
+#runit {
+#  logit "$@"
+#  $NOTREALLY "$@" >> $LOGFILE
+#
+#  if [[ $? -ne 0 ]]; then
+#    echo "Error executing call, exiting"
+#    exit 1
+#  fi
+#}
+#
+# Something like the below to handle task calls
+#
+# task { runit $TASK $TASK_OPTS "$@" ; }
+
+# If you want to get more complicated with ansi codes then an external library
+# will be necessary. See
+# https://github.com/harleypig/dotfiles/blob/master/.bash_functions.d/ansi for
+# ideas. There are other frameworks kicking around the web.
+
+# XXX: Are ready and pending counts supposed to have no context?
+
+__pending_count () { $TASK $TASK_OPTS rc.context: +PENDING count        ; }
+__rc_context    () { $TASK _get rc.context                              ; }
+__ready_count   () { $TASK $TASK_OPTS rc.context: +READY +PENDING count ; }
+
+__context_text  () { echo ${CONTEXT:=None} ; }
+__filter_text   () { echo ${FILTER:=None}  ; }
+
+__schedule_text () {
+
+  SCHEDULE_TEXT='Schedule'
+
+  [[ $RESCHEDULE -eq 1 ]] && SCHEDULE_TEXT='Re-schedule';
+  [[ $UNSCHEDULE -eq 1 ]] && SCHEDULE_TEXT='Un-schedule';
+
+  echo $SCHEDULE_TEXT
+
+}
+
+__header_text () {
+
+  HEADER_TEXT='<----- Taskwarrior'
+  [[ $BATCH_MODE -eq 1 ]] &&  HEADER_TEXT="${HEADER_TEXT} Batch"
+  HEADER_TEXT="${HEADER_TEXT} $(__schedule_text) ----->"
+
+  echo $HEADER_TEXT
+
+}
+
+header_text () {
+
+  # XXX: Allow option to clear screen each time header is called.
+
+  echo -e "
+${BOLD}${GREEN_FG} $(__header_text) ${RESET}
+  ${BOLD}${BLACK_FG}$(__ready_count) ready of $(__pending_count) pending tasks${RESET}
+$DIVIDER
+ Context: $(__context_text)
+ Filter : $(__filter_text)
+$DIVIDER
+"
+
+}
 
 ##################################################################################
 # Option Handling
+
+# XXX: Move this section to a function.
 
 DBG=0
 BATCH_MODE=0
 BATCH_LIMIT=$($TASK _get rc.sched.batch.limit)
 RESCHEDULE=0
 UNSCHEDULE=0
-CONTEXT_CURRENT=$($TASK _get rc.context)
-
-PROMPT_TEXT='Schedule'
 
 OPTIND=1 # This forces the index to 1. This isn't usually necessary, but I'm
          # a belt and suspenders guy.
@@ -106,11 +201,10 @@ if [[ $RESCHEDULE -eq 1 && $UNSCHEDULE -eq 1 ]]; then
   usage "re-schedule-mode and un-schedule-mode are mutually exclusive"
 fi
 
-[[ $RESCHEDULE -eq 1 ]] && PROMPT_TEXT='Re-schedule';
-[[ $UNSCHEDULE -eq 1 ]] && PROMPT_TEXT='Un-schedule';
+#if [[ $BATCH_MODE -eq 0 ]] &&
 
 if [[ -z $CONTEXT ]]; then
-  CONTEXT=$CONTEXT_CURRENT
+  CONTEXT=$(__rc_context)
 elif [[ ${CONTEXT,,} == 'none' ]]; then
   CONTEXT=
 elif [[ -z $($TASK _context | grep "^$CONTEXT$") ]]; then
@@ -134,130 +228,103 @@ FILTER="$@"
 ##################################################################################
 # Variable Setup
 
-WIDTH=90
-TASK_OPTS="rc.verbose: rc.defaultwidth:${WIDTH}"
 
 DIVIDER='-------------------------------------'
 
 CAND_LIST_LIMIT=$($TASK $TASK_OPTS _get rc.sched.cand.list.limit)
 CAND_COUNT=$($TASK $TASK_OPTS tag.not:nosch rc.context:$CONTEXT $FILTER +READY count)
-PENDING_COUNT=$($TASK $TASK_OPTS rc.context: +PENDING count)
-READY_COUNT=$($TASK $TASK_OPTS rc.context: +READY +PENDING count)
 SCHED_NONE_COUNT=$($TASK $TASK_OPTS +PENDING $FILTER +READY scheduled.none: count)
 SCHED_OLD_COUNT=$($TASK $TASK_OPTS +PENDING $FILTER +READY scheduled.before:today count)
 TARGET_LIST_LIMIT=$($TASK $TASK_OPTS _get rc.sched.target.list.limit)
 TARGET_COUNT=$($TASK $TASK_OPTS $FILTER +PENDING scheduled.after:now count)
 TARGET_NEXT_ID=$($TASK $TASK_OPTS rc.report.sch_target.columns=id rc.report.sch_target.labels= limit:1 sch_target)
 
+#$SCHED_NONE_COUNT' unscheduled, '$SCHED_OLD_COUNT' stale'
+
 # TODO fix the following:
 DATE_FMT=rc.dateformat.report=\'$($TASK _get rc.sched.datefmt)\'  #for target rpt
 
-PROMPT_BATCH=
-
-if [[ $BATCH_MODE -eq 1 ]]; then
-  PROMPT_BATCH=$CAND_COUNT' tasks'
-fi
-
-## COLORS
-declare -A title=([value]="[1;32m" [alt]="[38;5;10m[48;5;232m" [end]="[0m")
-    C1=${title[value]}  # bold green
-    C1a=${title[alt]}  # bold green on gray
-
-declare -A heading=([value]="[1;37m" [alt]="^[[38;5;242m" [end]="[0m")
-    C2=${heading[value]}  #bold white
-
-declare -A comment=([value]="[38;5;245m" [alt]="^[[38;5;242m" [end]="[0m")
-    C3=${comment[value]}  #gray13
-
-declare -A warning=([value]="[32;40m" [alt]="^[[38;5;242m" [end]="[0m")  # yellow
-    Cx=${title[end]}  #end color rule
+# This isn't used anywhere.
+#PROMPT_BATCH=
+#
+#if [[ $BATCH_MODE -eq 1 ]]; then
+#  PROMPT_BATCH=$CAND_COUNT' tasks'
+#fi
 
 ##################################################################################
-## Report
-### Header
+# main
 
-HEADER_TEXT='<----- Taskwarrior'
-[[ $BATCH_MODE -eq 1 ]] &&  HEADER_TEXT="${HEADER_TEXT} Batch"
-HEADER_TEXT="${HEADER_TEXT} ${PROMPT_TEXT} ----->"
-
-echo
-echo -e "${C1} ${HEADER_TEXT} ${Cx}"
-echo -e "${C3}   ${READY_COUNT} ready of ${PENDING_COUNT} pending tasks${Cx}"
-
-#$SCHED_NONE_COUNT' unscheduled, '$SCHED_OLD_COUNT' stale'
-
-##################################################################################
-### Context & Filter
-#### Context
-
-MSG=
-[[ -n $CONTEXT ]] && MSG="${MSG} Context\t: $CONTEXT\n"
-[[ -n $FILTER ]]  && MSG="${MSG} Filter\t: $FILTER\n"
-[[ -n $MSG ]]     && MSG="${MSG} No filters or context\n"
-
-echo -e "$DIVIDER\n$MSG"
+header_text
 
 ##################################################################################
 ## Candidates list
 
-echo -e $DIVIDER
+#candidates_list () {
+#
+#  if [[ $BATCH_MODE -eq 0 ]]; then
+#    if [[ $CAND_COUNT -eq 0 ]]; then
+#      echo -e ' No tasks match! Try changing the context or filter'
+#      exit 1
+#    else
+#      echo -e "${BOLD}${WHITE_FG} Next of ${CAND_COUNT} Candidates${RESET}"
+#      $TASK rc.verbose=label rc.report.sch_cand.columns=project,tags,description,urgency rc.context:$CONTEXT $FILTER limit:1 sch_cand;
+#    fi
+#
+#  else
+#    echo -e "${BOLD}${WHITE_FG} ${CAND_COUNT} Candidates${RESET}"
+#    $TASK rc.verbose=label rc.context:$CONTEXT $FILTER limit:$CAND_LIST_LIMIT sch_cand;
+#
+#    if [[ "$CAND_COUNT" -gt "$CAND_LIST_LIMIT" ]]; then
+#      CAND_MORE=$(( $CAND_COUNT - $CAND_LIST_LIMIT ))
+#      echo -e "${BOLD}${BLACK_FG}   (and $CAND_MORE more) are matching, unscheduled tasks${RESET}"
+#    else
+#      echo -e "${BOLD}${BLACK_FG}   these are matching, unscheduled tasks${RESET}"
+#    fi
+#  fi
+#
+#  echo -e $DIVIDER
+#
+#}
 
-if [[ $BATCH_MODE -eq 0 ]]; then
-  if [[ $CAND_COUNT -eq 0 ]]; then
-    echo -e ' No tasks match! Try changing the context or filter'
-    exit 1
-  else
-    echo -e "${C2} Next of ${CAND_COUNT} Candidates${Cx}"
-    $TASK rc.verbose=label rc.report.sch_cand.columns=project,tags,description,urgency rc.context:$CONTEXT $FILTER limit:1 sch_cand;
-  fi
-
-else
-  echo -e "${C2} ${CAND_COUNT} Candidates${Cx}"
-  $TASK rc.verbose=label rc.context:$CONTEXT $FILTER limit:$CAND_LIST_LIMIT sch_cand;
-
-  if [[ "$CAND_COUNT" -gt "$CAND_LIST_LIMIT" ]]; then
-    CAND_MORE=$(( $CAND_COUNT - $CAND_LIST_LIMIT ))
-    echo -e $C3'   (and '$CAND_MORE' more) are matching, unscheduled tasks'$Cx;
-  else
-    echo -e $C3'   these are matching, unscheduled tasks'$Cx
-  fi
-fi
-
-echo -e $DIVIDER
+#candidates_list
 
 ##################################################################################
 ## Targets list
-    if [[ "$TARGET_COUNT" == '' ]]; then
+if [[ "$TARGET_COUNT" == '' ]]; then
   echo -e ' No matching targets found';
-    else
-  echo $C2' '$TARGET_COUNT' Targets'$Cx
+else
+  echo "${BOLD}${WHITE_FG} ${TARGET_COUNT} Targets${RESET}"
 #  task rc.verbose:label rc.context:$CONTEXT $FILTER limit:$TARGET_LIST_LIMIT $DATE_FMT sch_target;
 #  task rc.verbose:label rc.context:$CONTEXT $FILTER limit:$TARGET_LIST_LIMIT rc.dateformat.report='a, b D, H:n' sch_target;
   $TASK rc.verbose:label rc.context:$CONTEXT $FILTER limit:$TARGET_LIST_LIMIT sch_target;
-    fi
-    if [[ "$TARGET_COUNT" -gt "$TARGET_LIST_LIMIT" ]]; then
+fi
+
+if [[ "$TARGET_COUNT" -gt "$TARGET_LIST_LIMIT" ]]; then
   TARGETS_MORE=$(( $TARGET_COUNT - $TARGET_LIST_LIMIT ))
-  echo -e $C3' and '$TARGETS_MORE' more) are possible scheduling target dates'$Cx;
-    fi
+  echo -e "${BOLD}${BLACK_FG} (and ${TARGETS_MORE} more) are possible scheduling target dates${RESET}"
+fi
 
 ##################################################################################
 ## Prompt
 echo -e $DIVIDER
 if [[ $BATCH_MODE -eq 1 && $CAND_COUNT -gt $BATCH_LIMIT ]];then
     echo -e '    '$CAND_COUNT' tasks is over the batch-limit of '$BATCH_LIMIT
-    echo -e $C3'    try changing the filter and/or context,
-    or override with "-l N", or change it in sched.rc'$Cx
+    echo -e "${BOLD}${BLACK_FG}    try changing the filter and/or context,
+    or override with '-l N', or change it in sched.rc${RESET}"
     echo
     exit 1
 fi
+
 if [[ $TARGET_COUNT -gt '0' ]]; then
-    echo -e $C3'   ID   [+|- offset]  eg: 142, 123 - 2dy, 234 + 2hr, 113 - 1wk'$Cx
+    echo -e "${BOLD}${BLACK_FG}   ID   [+|- offset]  eg: 142, 123 - 2dy, 234 + 2hr, 113 - 1wk${RESET}"
 fi
-    echo -e $C3'   date [+|- offset]  eg: mon, 15th, eom - 2dy, tomorrow + 14hr
+
+echo -e "${BOLD}${BLACK_FG}   date [+|- offset]  eg: mon, 15th, eom - 2dy, tomorrow + 14hr
    h[elp]    display USAGE text
-   q[uit]    quit without changes
- '$Cx
-    read -ep $C1' Schedule > '$Cx prompt
+   q[uit]    quit without changes${RESET}"
+
+read -ep "${BOLD}${GREEN_FG} Schedule > ${RESET}" prompt
+
 if [[ "$prompt" == '' ]] && [[ "$TARGET_NEXT_ID" == '' ]]; then
     echo ' No targets found, please enter a date'
     # TODO cycle prompt
